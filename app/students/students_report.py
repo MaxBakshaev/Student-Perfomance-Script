b9430f2bdcdf582b815f08124d9ee2bb06b5eeb2
Maxim Bakshaev <risen.2011@mail.ru>
2025-09-21 22:16:41 +0900

    Функция get_students_average_marks_table разбита на подфункции, добавлены тесты для функций collect_grades, calculate_avg_grades, sort_and_number (#8)

--- app/students/students_report.py
+++ app/students/students_report.py
@@ -2,16 +2,46 @@ from collections import defaultdict
 from typing import Dict, List, Tuple
 
 
+def collect_grades(rows_list: List[List[str]]) -> Dict[str, List[float]]:
+    """Добавляет в словарь имя и оценки"""
+
+    grades = defaultdict(list)
+    for row in rows_list:
+        name = row[0]  # имя студента
+        grade = int(row[4])  # оценка int
+        grades[name].append(grade)
+    return grades
+
+
+def calculate_avg_grades(
+    grades_dict: Dict[str, List[float]],
+) -> List[Tuple[str, float]]:
+    """Считает среднюю оценку для каждого студента"""
+
+    return [
+        (name, round(sum(grades) / len(grades), 2))
+        for name, grades in grades_dict.items()
+    ]
+
+
+def sort_and_number(
+    avg_grades: List[Tuple[str, float]],
+) -> List[Tuple[int, str, float]]:
+    """Cортирует и нумерует строки"""
+
+    sorted_ = sorted(avg_grades, key=lambda x: x[1], reverse=True)
+
+    return [(i + 1, name, avg) for i, (name, avg) in enumerate(sorted_)]
+
+
 def get_students_average_marks_table(
     rows_list: List[List[str]],
 ) -> List[Tuple[int, str, float]]:
     """
-    Считает среднюю оценку по каждому студенту
-
     Возвращает список кортежей (номер, имя, средняя оценка)
-    в порядке убывания оценок
+    в порядке в порядке успеваемости студентов
 
-    Пример:
+    Пример возвращаемого значения:
     table = [
         (1, 'Калинина Ольга', 4.8),
         (2, 'Максимов Кирилл', 4.4),
@@ -22,25 +52,12 @@ def get_students_average_marks_table(
     """
 
     # добавление в словарь имени и оценки
-    student_grades: Dict[str, list[float]] = defaultdict(list)
-    for row in rows_list:
-        name = row[0]  # имя студента
-        grade = int(row[4])  # оценка int
-        student_grades[name].append(grade)
+    grades_dict = collect_grades(rows_list)
 
     # подсчет средней оценки для каждого студента
-    avg_grades = [
-        (name, round(sum(grades) / len(grades), 2))
-        for name, grades in student_grades.items()
-    ]
+    avg_grades = calculate_avg_grades(grades_dict)
 
-    # сортировка по убыванию средней оценки
-    avg_grades_sorted = sorted(avg_grades, key=lambda x: x[1], reverse=True)
+    # сортировка и нумерация
+    table = sort_and_number(avg_grades)
 
-    # добавление нумерации
-    table = [
-        (i + 1, name, avg)
-        for i, (name, avg)
-        in enumerate(avg_grades_sorted)
-    ]
     return table
